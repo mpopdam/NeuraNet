80b5436e9797ba268b490351fb4a5145b2c9336f
Martin Opdam <martin.opdam@avivasolutions.nl>
2017-11-21 21:36:40 +0100

    Solve the XOR problem with the network

---
+++ Source/NeuraNet.Specs/XORNetworkLayout.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+using NeuraNet.Activations;
+using NeuraNet.NetworkLayout;
+
+namespace NeuraNet.Specs
+{
+    public class XORNetworkLayout : NetworkLayoutProvider
+    {
+        public XORNetworkLayout()
+        {
+            var neuronInitializer = new RandomLayerInitializer();
+            var outputActivation = new SigmoidActivation();
+
+            layers = new List<Layer>
+            {
+                new Layer(2, 3, neuronInitializer, outputActivation),
+                new Layer(3, 1, neuronInitializer, outputActivation),
+            };
+
+            ConnectLayers();
+        }
+    }
+}
